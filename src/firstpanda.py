f592a6d0681cb871c5cc287f6feff8206f5a41e4
Owain Roberts <ojrdude@hotmail.com>
2016-12-01 11:36:27 +0000

    Made panda walk about

--- src/firstpanda.py
+++ src/firstpanda.py
@@ -1,7 +1,10 @@
+
+from direct.actor.Actor import Actor
+from direct.interval.MetaInterval import Sequence
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from math import pi, sin, cos
-from direct.actor.Actor import Actor
+from panda3d.core import Point3  # pylint: disable=E0611
 
 
 class MyApp(ShowBase):
@@ -21,6 +24,26 @@ class MyApp(ShowBase):
         self.pandaActor.setScale(0.005, 0.005, 0.005)
         self.pandaActor.reparentTo(self.render)
         self.pandaActor.loop('walk')
+
+        pandaPosInterval1 = self.pandaActor.posInterval(13,
+                                                        Point3(0, -10, 0),
+                                                        startPos=Point3(0, 10, 0))
+        pandaPosInterval2 = self.pandaActor.posInterval(13,
+                                                        Point3(0, 10, 0),
+                                                        startPos=Point3(0, -10, 0))
+        pandaHprInterval1 = self.pandaActor.hprInterval(3,
+                                                        Point3(180, 0, 0),
+                                                        startHpr=Point3(0, 0, 0))
+        pandaHprInterval2 = self.pandaActor.hprInterval(3,
+                                                        Point3(0, 0, 0),
+                                                        startHpr=Point3(180, 0, 0))
+
+        self.pandaPace = Sequence(pandaPosInterval1,
+                                  pandaHprInterval1,
+                                  pandaPosInterval2,
+                                  pandaHprInterval2,
+                                  name='pandaPace')
+        self.pandaPace.loop()
 
     def spinCameraTask(self, task):
         angleDegrees = task.time * 6.0
