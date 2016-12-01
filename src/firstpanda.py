94746d49d6802e2abef4af8ee63927d9ba0f3eb7
Owain Roberts <ojrdude@hotmail.com>
2016-12-01 11:08:32 +0000

    Scene drawn and camera spinning

---
+++ src/firstpanda.py
@@ -0,0 +1,27 @@
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+from math import pi, sin, cos
+
+
+class MyApp(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.scene = self.loader.loadModel('models/environment')
+        self.scene.reparentTo(self.render)
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+        self.taskMgr.add(self.spinCameraTask, 'SpinCameraTask')
+
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
+
+
+app = MyApp()
+app.run()
