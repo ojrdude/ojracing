e1021e16a432f13f56d3c6879b9c5570ad0445b2
Owain Roberts <ojrdude@hotmail.com>
2017-01-08 18:27:23 +0000

    Added pylintrc, made game camera fix behind player car.

--- src/game.py
+++ src/game.py
@@ -2,8 +2,12 @@
 Main game entry point
 """
 from direct.showbase.ShowBase import ShowBase
+from direct.task.Task import Task
+from math import cos, sin, radians
 
 
+# pylint: disable = E1101
+# Type guessing wrong
 class Game(ShowBase):
     """
     The Class that starts the game
@@ -12,7 +16,7 @@ class Game(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
-#         self.disableMouse()
+        self.disableMouse()
         self.scene = self.loader.loadModel('models/track1')
         self.scene.reparentTo(self.render)
 
@@ -20,6 +24,27 @@ class Game(ShowBase):
         self.car.setPos(0, 50, 1)
         self.car.setHpr(90, 0, 0)
         self.car.reparentTo(self.render)
+
+        self.taskMgr.add(self.alignCameraBehindCar, "Align Camera Behind Car")
+
+    def alignCameraBehindCar(self, task):
+        """
+        Ensure that the player's view is behind the car.
+        """
+        carXpos, carYpos, carZpos = self.car.getPos()
+        carHeadingDegrees, carPitchDegrees, carRollDegrees = self.car.getHpr()
+        cameraHeadingDegrees = carHeadingDegrees
+        cameraPitchDegrees = carPitchDegrees
+        cameraRollDegrees = carRollDegrees
+
+        carHeadingRadians = radians(carHeadingDegrees)
+
+        cameraXpos = carXpos + 10 * sin(carHeadingRadians)
+        cameraYpos = carYpos - 10 * cos(carHeadingRadians)
+        self.camera.setPos(cameraXpos, cameraYpos, carZpos + 1)
+        self.camera.setHpr(
+            cameraHeadingDegrees, cameraPitchDegrees, cameraRollDegrees)
+        return Task.cont
 
 if __name__ == '__main__':
     game = Game()
